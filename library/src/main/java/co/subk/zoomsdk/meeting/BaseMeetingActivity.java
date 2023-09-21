304e75152b31db9c952470c083c06f1588bf22b1
Arul <arul.harsh@volgainfotech.com>
2023-09-21 12:49:32 +0530

    Minor Changes in Sending DAta with event bus

--- library/src/main/java/co/subk/zoomsdk/meeting/BaseMeetingActivity.java
+++ library/src/main/java/co/subk/zoomsdk/meeting/BaseMeetingActivity.java
@@ -478,9 +478,10 @@ public class BaseMeetingActivity extends AppCompatActivity implements ZoomVideoS
         }
 
         /** Added by arul for location functionality - 16/09/23*/
-        if (checkPermissions()) {
+
+       /* if (checkPermissions()) {
             getLastLocation();
-        }
+        }*/
     }
 
     protected void resumeSubscribe() {
@@ -2406,6 +2407,8 @@ public class BaseMeetingActivity extends AppCompatActivity implements ZoomVideoS
         @Override
         public void onLocationResult(LocationResult locationResult) {
             Location mLastLocation = locationResult.getLastLocation();
+            EventBus.getDefault().post(new SubkEvent("location ",String.valueOf(mLastLocation.getLatitude()),String.valueOf(mLastLocation.getLongitude()),String.valueOf(mLastLocation.getAccuracy())));
+
             /** Need to add a event here as well @ arul  */
             /*latitudeTextView.setText("Latitude: " + mLastLocation.getLatitude() + "");
             longitTextView.setText("Longitude: " + mLastLocation.getLongitude() + "");*/
