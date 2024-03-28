2fc63bf137b61828c3f6cb4b8c0d6fba743bbdff
piyush <piyush.d@onefigura.com>
2024-03-28 16:17:50 +0530

    - answer model update with serialized

--- library/src/main/java/co/subk/zoomsdk/meeting/models/Answer.java
+++ library/src/main/java/co/subk/zoomsdk/meeting/models/Answer.java
@@ -1,27 +1,17 @@
 package co.subk.zoomsdk.meeting.models;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Answer {
+    @SerializedName("id")
     private String id;
+
+    @SerializedName("answer")
     private String answer;
 
-    public Answer(String questionId, String answer) {
-        this.id = questionId;
-        this.answer = answer;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
+    public Answer(String id, String answer) {
         this.id = id;
-    }
-
-    public String getAnswer() {
-        return answer;
-    }
-
-    public void setAnswer(String answer) {
         this.answer = answer;
     }
+
 }
